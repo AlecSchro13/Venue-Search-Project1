a367dd8efea119205ab47980ef69eb1bf074015b
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-08-06 18:09:54 -0500

    worked in javascript to display the event name, venue, genre, date, and time. added span to event html

--- assets/js/event.js
+++ assets/js/event.js
@@ -23,7 +23,25 @@ function fetchEventInfo(eventId) {
       return response.json();
     })
     .then((data) => {
-      console.log(data)
+      console.log(data);
+      let eventName = data.name;
+      //console.log(data.name);
+        document.getElementById("eventName").textContent = eventName;
+      let venueName = data._embedded.venues[0].name;
+      //console.log(data._embedded.venues[0].name);
+        document.getElementById("venueName").textContent = venueName;
+      let address = data._embedded.venues[0].address.line1;
+        //console.log(data._embedded.venues[0].address.line1);
+        document.getElementById("address").textContent = address;
+      let genre = data.classifications[0].genre.name;
+      //console.log(data.classifications[0].genre.name);
+        document.getElementById("genre").textContent = genre;
+      let eventDate = data.dates.start.localDate;
+      //console.log(data.dates.start.localDate);
+        document.getElementById("eventDate").textContent = eventDate
+      let startTime = data.dates.start.localTime;
+      //console.log(data.dates.start.localTime);
+        document.getElementById("startTime").textContent = startTime
     })
     .catch((e) => {
         console.log(e)
